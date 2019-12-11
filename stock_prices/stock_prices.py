3c4a2ca7a8c2039f97610d010b06884614060865
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-11 15:33:11 -0700

    finished stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,46 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  maxi = max(prices)
+  mini = min(prices)
+  test = 1
+  if prices.index(maxi) > prices.index(mini):
+    return maxi - mini
+  else: 
+    counter = -5000
+    for i in range(len(prices)-1):
+      for j in range(test, len(prices)):
+        cprice = -prices[i] + prices[j]
+        if cprice > counter:
+          counter = cprice
+      test+=1
+    print("counter", counter)
+    return counter
+  
+
+  # for i in range(len(prices)):
 
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+print(find_max_profit([10, 7, 5, 8, 11, 9]))
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+
+# if __name__ == '__main__':
+#   # This is just some code to accept inputs from the command line
+#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
+#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+#   args = parser.parse_args()
+
+#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+# elif prices.index(maxi) < prices.index(mini):
+#     nprices = prices[prices.index(maxi)+1:]
+#     print('nprices', nprices)
+#     nmaxi = max(nprices)
+#     if nprices.index(nmaxi) > nprices.index(mini):
+#       return nmaxi - mini
+#     elif nprices.index(nmaxi) < nprices.index(mini):
+#       mprices = nprices[:]
+#       nmini = min(mprices)
+#       if mprices.index(nmaxi) > mprices.index(nmini):
+#         return nmaxi - mini 
