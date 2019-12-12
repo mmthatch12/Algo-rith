7227f7ed24a545d1204371600e5d10759f694877
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-12 09:24:28 -0700

    working on cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,11 +6,16 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n <= 0:
+    return 0
+  
+  return 
+  
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,34 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batchdict = {}
+  for i in recipe:
+    batchdict[i] = 0
+  print(batchdict)
+  
+  
+  if len(recipe) > len(ingredients):
+    return 0
+  else:
+    enough = True
+    while enough:
+      for i in recipe:
+        if recipe[i] < ingredients[i]:
+          batchdict[i]+=1
+          ingredients[i] -= recipe[i]
+        else:
+          enough = False
+    print("2", batchdict)
+    batcharr = list(batchdict.values())
+    return min(batcharr)
+
+print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }))
+  
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
