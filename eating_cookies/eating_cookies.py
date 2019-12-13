f8a99bfafb75e617b8156a0e217b626a96040436
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-13 08:32:23 -0700

    fib work

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,12 +5,21 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  if n <= 0:
-    return 0
+cachee = {}
+def eating_cookies(n):
   
-  return 
+  if n <= 1:
+    return 1
+  if n == 2:
+    return 2
+
+  if n not in cachee:
+    cachee[n] = eating_cookies(n-1)+eating_cookies(n-2)+eating_cookies(n-3)
   
+  return eating_cookies(n-1)+eating_cookies(n-2)+eating_cookies(n-3)
+
+
+print(eating_cookies(30))
 
 
 # if __name__ == "__main__":
@@ -19,3 +28,60 @@ def eating_cookies(n, cache=None):
 #     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
 #   else:
 #     print('Usage: eating_cookies.py [num_cookies]')
+
+# if n = 0 => 1
+# if n = 1 => 1
+# if n = 2 => 2
+# if n = 3 => 4
+# if n = 4 => 7
+# combo of ones can only happen in one way
+# if n is even combo of 2 can only happen 1 way, else no ways
+# if n is divisible by 3 combo of 3 can only happen 1 way, else no ways
+
+# n=1
+# 1
+
+# n=2
+# 11
+# 2
+
+# n = 3
+# 111
+# 21
+# 12
+# 3
+
+# n=4
+# 1111
+# 211
+# 121
+# 31
+# 112
+# 22
+# 13
+
+
+# n = 4 => 7
+# only ones = 1[1,1,1,1]
+# only twos = 1[2,2]
+# ones and twos = 3[1,1,2][1,2,1][2,1,1]
+# ones and threes = 2[3,1][1,3]
+
+# n= 5 => 13
+
+# only ones= only ever on way 1 [1,1,1,1,1]
+# only twos and ones =      7 [2,1,1,1][1,2,1,1][1,1,2,1][1,1,1,2][2,2,1][2,1,2][1,2,2]
+# only ones and threes = 3 [1,3,1][1,1,3][3,1,1]
+# only twos and threes = 2 [3,2][2,3]
+
+# n = 6 => 22
+
+# only ones = 1 [1,1,1,1,1,1]
+# only twos = 1 [2,2,2]
+# only threes = 1[3,3]
+# only ones and twos = 10[1,1,1,1,2][1,1,1,2,1][1,1,2,1,1][1,2,1,1,1][2,1,1,1,1][2,2,1,1][2,1,1,2][1,1,2,2][1,2,1,2][2,1,2,1]
+# only ones and threes = 4[3,1,1,1][1,1,1,3][1,1,3,1][1,3,1,1]
+# only twos and threes = 0
+# all = 5[3,2,1][1,2,3][2,1,3][3,1,2][1,3,2]
+
+
--- lecture/fibonacci.py
+++ lecture/fibonacci.py
@@ -14,33 +14,42 @@
 #         return cache[n]
 #     return fibn(n)
 
-def otherway(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    prevprev = 0
-    prev = 1
-
-    for _ in range(n-1):
-        cur = prevprev + prev
-        prevprev = prev
-        prev = cur
-    return cur
-print(otherway(450))
-
-
-
-# cache = {} #memorization
-# def fib(n):
+# def otherway(n):
 #     if n == 0:
 #         return 0
 #     elif n == 1:
 #         return 1
-#     if n not in cache:
-#         cache[n] = fib(n-1) + fib(n-2)
+#     prevprev = 0
+#     prev = 1
 
-#     return fib(n-1) + fib(n-2)
+#     for _ in range(n-1):
+#         cur = prevprev + prev
+#         prevprev = prev
+#         prev = cur
+#     return cur
+# print(otherway(450))
+
+
+
+cache = {} #memorization
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    if n not in cache:
+        cache[n] = fib(n-1) + fib(n-2)
+
+    return cache[n]
+
+print(fib(50))
+
+# def orfib(n):
+#     if n==0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     return fib(n-1)+fib(n-2)
 
 
 
